7af60ee3d70f0eb97d9d86f9ece6a7c8340a7d92
김순홍 <shong1332@naver.com>
2021-07-05 15:21:21 +0900

    first commit

---
+++ class/pages/boards/new/index.js
@@ -0,0 +1,11 @@
+export default function aaa() {
+
+
+
+
+    return (
+    <div>게시판 입니다.</div>
+    
+    
+    )
+}
---
+++ class/styles/Home.styles.js
@@ -0,0 +1,21 @@
+import styled from '@emotion/styled'
+
+export const Title = styled.h1`
+    
+    background-color: red;
+    margin-left: 50%;
+    
+    
+`
+
+export const Name = styled.div`
+    font-size: 10px;
+`
+export const Wrapper = styled.div`
+`
+
+export const Myinput = styled.input`
+`
+
+export const Ddd = styled.input`
+`
