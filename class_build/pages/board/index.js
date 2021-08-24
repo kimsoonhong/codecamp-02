f7f7cacbe48520caeeb72e4967b0d535b66a8b0b
김순홍 <shong1332@naver.com>
2021-08-24 12:10:52 +0900

    2021.08.24

--- class_build/pages/board/index.js
+++ class_build/pages/board/index.js
@@ -1,17 +1,16 @@
-import head from "next/head";
+import Head from "next/head";
 
 export default function boardPage() {
 	return (
 		<>
-			<head>
-				<mata property="og:title" content="정적 게시판" />
-				<mata property="og:description" content="이 주소는 정적 주소입니다." />
-				<mata
+			<Head>
+				<meta property="og:title" content="정적 게시판" />
+				<meta property="og:description" content="이 주소는 정적 주소입니다." />
+				<meta
 					property="og:image"
 					content="https://mblogthumb-phinf.pstatic.net/MjAxNzEyMjZfMjIx/MDAxNTE0MjU2NDUxOTMw.ImgF9jtMTl9FCT5CnlfBh7GAAUJS-TFRi8pnTq5MTScg.Ht5wFhUVufVdtxilpnjDI0xd_3WmNcbQNnh_IYI3jnIg.JPEG.soon7268/IMG_9259.jpg?type=w800gi"
 				/>
-			</head>
-
+			</Head>
 			<div>여기는 게시판 입니다.</div>
 		</>
 	);
--- class_build/pages/market/[useditemId]/index.js
+++ class_build/pages/market/[useditemId]/index.js
@@ -1,3 +1,41 @@
-export default function marketPage() {
-	return <div>마켓입니다.</div>;
+import { request, gql } from "graphql-request";
+
+import Head from "next/head";
+
+const FETCH_USEDITEM = gql`
+	query fetchUseditem($useditemId: ID!) {
+		fetchUseditem(useditemId: $useditemId) {
+			name
+			remarks
+			images
+		}
+	}
+`;
+
+export default function marketPage(props) {
+	return (
+		<>
+			<Head>
+				<meta property="og:title" content={props.fetchUseditem.name} />
+				<meta property="og:title" content={props.fetchUseditem.remarks} />
+				<meta
+					property="og:title"
+					content={`https://storage.googleapis.com/${props.fetchUseditem.images[0]}`}
+				/>
+			</Head>
+			<div>마켓입니다.</div>
+		</>
+	);
 }
+
+export const getServerSideProps = async (context) => {
+	const result = await request(
+		"https://backend02.codebootcamp.co.kr/graphql",
+		FETCH_USEDITEM,
+		{
+			useditemId: "context.query.useditemId",
+		}
+	);
+
+	return { props: { fetchUseditem: result.fetchUseditem } };
+};
