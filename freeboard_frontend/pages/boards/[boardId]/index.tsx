819fae61e0accbf1af20c7cf74eeeff2c8b86718
김순홍 <shong1332@naver.com>
2021-09-15 22:55:39 +0900

    5

--- freeboard_frontend/pages/boards/[boardId]/index.tsx
+++ freeboard_frontend/pages/boards/[boardId]/index.tsx
@@ -1,23 +1,23 @@
-// import { request, gql } from "graphql-request";
-// import BoardDetail from "../../../src/components/units/board/detail/BoardDetail.container";
-// import BoardCommentList from "../../../src/components/units/boardComment/list/BoardCommentList.container";
-// import BoardCommentWrite from "../../../src/components/units/boardComment/write/BoardCommentWrite.container";
-// import Head from "next/head";
+import { request, gql } from "graphql-request";
+import BoardDetail from "../../../src/components/units/board/detail/BoardDetail.container";
+import BoardCommentList from "../../../src/components/units/boardComment/list/BoardCommentList.container";
+import BoardCommentWrite from "../../../src/components/units/boardComment/write/BoardCommentWrite.container";
+import Head from "next/head";
 
-// export const FETCH_BOARD = gql`
-//   query fetchBoard($boardId: ID!) {
-//     fetchBoard(boardId: $boardId) {
-//       title
-//       contents
-//       images
-//     }
-//   }
-// `;
+export const FETCH_BOARD = gql`
+  query fetchBoard($boardId: ID!) {
+    fetchBoard(boardId: $boardId) {
+      title
+      contents
+      images
+    }
+  }
+`;
 
 export default function BoardsDetailPage(props: any) {
   return (
     <>
-      {/* <Head>
+      <Head>
         <meta property="og:title" content={props.fetchBoard.title} />
         <meta property="og:contents" content={props.fetchBoard.contents} />
         <meta
@@ -27,19 +27,18 @@ export default function BoardsDetailPage(props: any) {
       </Head>
       <BoardDetail />
       <BoardCommentWrite />
-      <BoardCommentList /> */}
+      <BoardCommentList />
     </>
   );
 }
-// export const getServerSideProps = async (context: any) => {
-//   const result = await request(
+export const getServerSideProps = async (context: any) => {
+  const result = await request(
+    "https://backend02.codebootcamp.co.kr/graphql",
+    FETCH_BOARD,
+    {
+      boardId: context.query.boardId,
+    }
+  );
 
-//     "https://backend02.codebootcamp.co.kr/graphql",
-//     FETCH_BOARD,
-//     {
-//       boardId: context.query.boardId,
-//     }
-//   );
-
-//   return { props: { fetchBoard: result.fetchBoard } };
-// };
+  return { props: { fetchBoard: result.fetchBoard } };
+};
--- freeboard_frontend/pages/market/[useditemId]/index.tsx
+++ freeboard_frontend/pages/market/[useditemId]/index.tsx
@@ -1,23 +1,23 @@
-// import { request, gql } from "graphql-request";
-// import MarketDetail from "../../../src/components/units/market/detail/MarketDetail.container";
-// import QuestionList from "../../../src/components/units/marketComment/questionList/questionList.container";
-// import QuestionWrite from "../../../src/components/units/marketComment/questionWrite/questionWrite.container";
-// import Head from "next/head";
+import { request, gql } from "graphql-request";
+import MarketDetail from "../../../src/components/units/market/detail/MarketDetail.container";
+import QuestionList from "../../../src/components/units/marketComment/questionList/questionList.container";
+import QuestionWrite from "../../../src/components/units/marketComment/questionWrite/questionWrite.container";
+import Head from "next/head";
 
-// const FETCH_USEDITEM = gql`
-//   query fetchUseditem($useditemId: ID!) {
-//     fetchUseditem(useditemId: $useditemId) {
-//       name
-//       remarks
-//       images
-//     }
-//   }
-// `;
+const FETCH_USEDITEM = gql`
+  query fetchUseditem($useditemId: ID!) {
+    fetchUseditem(useditemId: $useditemId) {
+      name
+      remarks
+      images
+    }
+  }
+`;
 
 export default function BoardsDetailPage(props: any) {
   return (
     <>
-      {/* <Head>
+      <Head>
         <meta property="og:name" content={props.fetchUseditem.name} />
         <meta property="og:description" content={props.fetchUseditem.remarks} />
         <meta
@@ -27,19 +27,19 @@ export default function BoardsDetailPage(props: any) {
       </Head>
       <MarketDetail />
       <QuestionWrite />
-      <QuestionList /> */}
+      <QuestionList />
     </>
   );
 }
 
-// export const getServerSideProps = async (context: any) => {
-//   const result = await request(
-//     "https://backend02.codebootcamp.co.kr/graphql",
-//     FETCH_USEDITEM,
-//     {
-//       useditemId: context.query.useditemId,
-//     }
-//   );
+export const getServerSideProps = async (context: any) => {
+  const result = await request(
+    "https://backend02.codebootcamp.co.kr/graphql",
+    FETCH_USEDITEM,
+    {
+      useditemId: context.query.useditemId,
+    }
+  );
 
-//   return { props: { fetchUseditem: result.fetchUseditem } };
-// };
+  return { props: { fetchUseditem: result.fetchUseditem } };
+};
