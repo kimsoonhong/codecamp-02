d9f6a15568e670b0ec4e36e066f8522bd4c26449
김순홍 <shong1332@naver.com>
2021-09-15 22:12:34 +0900

    2021.09.15

--- freeboard_frontend/pages/boards/[boardId]/index.tsx
+++ freeboard_frontend/pages/boards/[boardId]/index.tsx
@@ -1,13 +1,45 @@
+import { request, gql } from "graphql-request";
 import BoardDetail from "../../../src/components/units/board/detail/BoardDetail.container";
 import BoardCommentList from "../../../src/components/units/boardComment/list/BoardCommentList.container";
 import BoardCommentWrite from "../../../src/components/units/boardComment/write/BoardCommentWrite.container";
+import Head from "next/head";
 
-export default function BoardsDetailPage() {
+export const FETCH_BOARD = gql`
+  query fetchBoard($boardId: ID!) {
+    fetchBoard(boardId: $boardId) {
+      title
+      contents
+      images
+    }
+  }
+`;
+
+export default function BoardsDetailPage(props: any) {
   return (
     <>
+      <Head>
+        <meta property="og:title" content={props.fetchBoard.title} />
+        <meta property="og:contents" content={props.fetchBoard.contents} />
+        <meta
+          property="og:images"
+          content={`https://storage.googleapis.com/${props.fetchBoard.images[0]}`}
+        />
+      </Head>
       <BoardDetail />
       <BoardCommentWrite />
       <BoardCommentList />
     </>
   );
 }
+export const getServerSideProps = async (context: any) => {
+  const result = await request(
+
+    "https://backend02.codebootcamp.co.kr/graphql",
+    FETCH_BOARD,
+    {
+      boardId: context.query.boardId,
+    }
+  );
+
+  return { props: { fetchBoard: result.fetchBoard } };
+};
--- freeboard_frontend/pages/market/[useditemId]/index.tsx
+++ freeboard_frontend/pages/market/[useditemId]/index.tsx
@@ -1,13 +1,45 @@
+import { request, gql } from "graphql-request";
 import MarketDetail from "../../../src/components/units/market/detail/MarketDetail.container";
 import QuestionList from "../../../src/components/units/marketComment/questionList/questionList.container";
 import QuestionWrite from "../../../src/components/units/marketComment/questionWrite/questionWrite.container";
+import Head from "next/head";
 
-export default function BoardsDetailPage() {
+const FETCH_USEDITEM = gql`
+  query fetchUseditem($useditemId: ID!) {
+    fetchUseditem(useditemId: $useditemId) {
+      name
+      remarks
+      images
+    }
+  }
+`;
+
+export default function BoardsDetailPage(props: any) {
   return (
     <>
+      <Head>
+        <meta property="og:name" content={props.fetchUseditem.name} />
+        <meta property="og:description" content={props.fetchUseditem.remarks} />
+        <meta
+          property="og:images"
+          content={`https://storage.googleapis.com/${props.fetchUseditem.images[0]}`}
+        />
+      </Head>
       <MarketDetail />
       <QuestionWrite />
       <QuestionList />
     </>
   );
 }
+
+export const getServerSideProps = async (context: any) => {
+  const result = await request(
+    "https://backend02.codebootcamp.co.kr/graphql",
+    FETCH_USEDITEM,
+    {
+      useditemId: context.query.useditemId,
+    }
+  );
+
+  return { props: { fetchUseditem: result.fetchUseditem } };
+};
