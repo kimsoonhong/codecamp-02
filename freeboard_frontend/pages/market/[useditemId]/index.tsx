aeae631b4bad47312833a7e616890ce7f049fec6
김순홍 <shong1332@naver.com>
2021-10-15 19:06:47 +0900

    ssr

--- freeboard_frontend/pages/market/[useditemId]/index.tsx
+++ freeboard_frontend/pages/market/[useditemId]/index.tsx
@@ -1,30 +1,30 @@
-// import { request, gql } from "graphql-request";
+import { request, gql } from "graphql-request";
 import MarketDetail from "../../../src/components/units/market/detail/MarketDetail.container";
 import QuestionList from "../../../src/components/units/marketComment/questionList/questionList.container";
 import QuestionWrite from "../../../src/components/units/marketComment/questionWrite/questionWrite.container";
-// import Head from "next/head";
+import Head from "next/head";
 
-// const FETCH_USEDITEM = gql`
-//   query fetchUseditem($useditemId: ID!) {
-//     fetchUseditem(useditemId: $useditemId) {
-//       name
-//       remarks
-//       images
-//     }
-//   }
-// `;
+const FETCH_USEDITEM = gql`
+  query fetchUseditem($useditemId: ID!) {
+    fetchUseditem(useditemId: $useditemId) {
+      name
+      remarks
+      images
+    }
+  }
+`;
 
 export default function BoardsDetailPage(props: any) {
   return (
     <>
-      {/* <Head>
+      <Head>
         <meta property="og:name" content={props.fetchUseditem.name} />
         <meta property="og:description" content={props.fetchUseditem.remarks} />
         <meta
           property="og:images"
           content={`https://storage.googleapis.com/${props.fetchUseditem.images[0]}`}
         />
-      </Head> */}
+      </Head>
       <MarketDetail />
       <QuestionWrite />
       <QuestionList />
@@ -32,14 +32,14 @@ export default function BoardsDetailPage(props: any) {
   );
 }
 
-// export const getServerSideProps = async (context: any) => {
-//   const result = await request(
-//     "https://backend02.codebootcamp.co.kr/graphql",
-//     FETCH_USEDITEM,
-//     {
-//       useditemId: context.query.useditemId,
-//     }
-//   );
+export const getServerSideProps = async (context: any) => {
+  const result = await request(
+    "https://backend02.codebootcamp.co.kr/graphql",
+    FETCH_USEDITEM,
+    {
+      useditemId: context.query.useditemId,
+    }
+  );
 
-//   return { props: { fetchUseditem: result.fetchUseditem } };
-// };
+  return { props: { fetchUseditem: result.fetchUseditem } };
+};
