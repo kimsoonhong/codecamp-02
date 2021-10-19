0595bbf5339ef9385d5b5d406c4b248d430723ff
김순홍 <shong1332@naver.com>
2021-10-19 11:25:50 +0900

    2021.10.19

--- freeboard_frontend/pages/boards/[boardId]/index.tsx
+++ freeboard_frontend/pages/boards/[boardId]/index.tsx
@@ -1,44 +1,44 @@
-// import { request, gql } from "graphql-request";
+import { request, gql } from "graphql-request";
 import BoardDetail from "../../../src/components/units/board/detail/BoardDetail.container";
 import BoardCommentList from "../../../src/components/units/boardComment/list/BoardCommentList.container";
 import BoardCommentWrite from "../../../src/components/units/boardComment/write/BoardCommentWrite.container";
-// import Head from "next/head";
+import Head from "next/head";
 
-// export const FETCH_BOARD = gql`
-//   query fetchBoard($boardId: ID!) {
-//     fetchBoard(boardId: $boardId) {
-//       title
-//       contents
-//       images
-//     }
-//   }
-// `;
+export const FETCH_BOARD = gql`
+  query fetchBoard($boardId: ID!) {
+    fetchBoard(boardId: $boardId) {
+      title
+      contents
+      images
+    }
+  }
+`;
 
 export default function BoardsDetailPage(props: any) {
   return (
     <>
-      {/* <Head>
+      <Head>
         <meta property="og:title" content={props.fetchBoard.title} />
         <meta property="og:contents" content={props.fetchBoard.contents} />
         <meta
           property="og:images"
           content={`https://storage.googleapis.com/${props.fetchBoard.images[0]}`}
         />
-      </Head> */}
+      </Head>
       <BoardDetail />
       <BoardCommentWrite />
       <BoardCommentList />
     </>
   );
 }
-// export const getServerSideProps = async (context: any) => {
-//   const result = await request(
-//     "https://backend02.codebootcamp.co.kr/graphql04",
-//     FETCH_BOARD,
-//     {
-//       boardId: context.query.boardId,
-//     }
-//   );
+export const getServerSideProps = async (context: any) => {
+  const result = await request(
+    "https://backend02.codebootcamp.co.kr/graphql04",
+    FETCH_BOARD,
+    {
+      boardId: context.query.boardId,
+    }
+  );
 
-//   return { props: { fetchBoard: result.fetchBoard } };
-// };
+  return { props: { fetchBoard: result.fetchBoard } };
+};
